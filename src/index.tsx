3bd0a196aaa37357fcdf0c56aec953de76c61b45
Rofiyanto Bagus P <pradiktabagus@gmail.com>
2021-07-30 17:09:38 +0700

    redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App/App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux"
+import {createStore} from "redux"
+import {rootReducer} from "./configureStore"
 
+const store = createStore(rootReducer)
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
