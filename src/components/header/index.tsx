7d63435dc1a21ff868a4633c6f1ecffa68aa6554
Rofiyanto Bagus P <pradiktabagus@gmail.com>
2021-08-23 10:55:04 +0700

    search component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import "./index.css"
 import { HelperConstanta } from "../../helper/constanta"
+import Search from '../search'
 interface HeaderProps {
     User: Object
 }
@@ -11,7 +12,7 @@ export const Index = (props: HeaderProps) => {
         <header>
             <nav className="navbar">
                 <img src={`${HelperConstanta.BASE_CDN}/v1629387080/sabana_nvpoct.svg`} alt="Sabana" className="logo" />
-                <div>search</div>
+                <Search placeholder="Search..."/>
                 <button className="opt-user">auth</button>
             </nav>
         </header>
--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -5,16 +5,25 @@ interface SearchProps {
     primary?: boolean,
     size?: 'small' | 'medium' | 'large';
     placeholder?: string,
-    onClick?: () => void;
+    onClick?: () => void,
+    searchBtn?: boolean,
 }
 export const Search = ({
     primary = false,
     size= 'medium',
     placeholder,
+    searchBtn = true,
     ...props
 }: SearchProps) => {
     return (
-        <input placeholder={placeholder} className={`sab-search`}/>
+        <div className={`${searchBtn ? "tw_flex" : ''} tw_relative`}>
+            <input placeholder={placeholder} className={`sab-search sab-textfield`}/>
+            {searchBtn ? (
+            <button className="tw_absolute search-btn">
+                <img src="https://img.icons8.com/ios/50/000000/search--v1.png" alt="search"/>
+            </button>) : null}
+            
+        </div>
     )
 }
 
