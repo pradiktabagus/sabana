427250236e3d8c6dca7de51df4b5243eacef898f
Rofiyanto Bagus Pradikta <pradiktabagus@gmail.com>
2021-09-02 08:14:03 +0700

    updated

--- src/containers/Profile/Index.tsx
+++ src/containers/Profile/Index.tsx
@@ -5,7 +5,13 @@ function Index(props: any) {
     return (
         <div className="profile-page">
             <div className="profile-cover">
-                
+                <div className="profile-banner">
+                    <div>
+                        <span>
+                            <img className="profile-picture" src="https://d2p5mifqdrbapa.cloudfront.net/photo-profile.jpg" alt="profile" />
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     )
